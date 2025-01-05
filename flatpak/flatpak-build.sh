fc44d0bed78afed31e48f9587b5d9c1a7b5ed81a
hydrian <ben.tyger@tygerclan.net>
2025-01-05 12:21:02 -0500

    Cleaning up flatpak-build.sh

--- flatpak/flatpak-build.sh
+++ flatpak/flatpak-build.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env -S bash -x
-FLATPAK_BIN=$(which flatpak)
-if [ $? -ne 0 ] ; then
-  echo "flatpak is not installed." 1>&2 
-  exit 2
-fi
+#!/usr/bin/env -S bash
 FLATPAK_BUILDER=$(which flatpak-builder)
 if [ $? -ne 0 ] ; then
   echo "flatpak-builder is not installed." 1>&2 
@@ -14,7 +9,19 @@ SCRIPT_FILE=$(realpath "${0}")
 GIT_ROOT_DIR=$(realpath "$(dirname "${SCRIPT_FILE}")/..") 
 echo "Building Flatpak image..."
 pushd "$GIT_ROOT_DIR" 1>/dev/null
-flatpak-builder .flatpak-build --ccache --force-clean --repo=./flatpak-repo  flatpak/net.azurewebsites.pathos.yml --install --user
+test ! -d .flatpak && mkdir -p .flatpak
+"${FLATPAK_BUILDER}" .flatpak/build \
+  --ccache \
+  --force-clean \
+  --state-dir=.flatpak/state \
+  --repo=.flatpak/repo  \
+  flatpak/net.azurewebsites.pathos.yml \
+  --install \
+  --user
+if [ $? -ne 0 ] ; then
+  echo "Failed to build image" 1>&2
+  exit 2
+fi
 popd 1>/dev/null
 echo "Flatpak image built successfully"
 exit 0
